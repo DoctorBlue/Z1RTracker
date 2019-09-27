17a7d1cb2614bbc4d885f75c34bebc67d00b9ade
Doctor Blue <scott.depouw+db@gmail.com>
2019-09-27 11:20:31 -0400

    Basic functionality.

--- Z1RTracker/Form1.Designer.cs
+++ Z1RTracker/Form1.Designer.cs
@@ -30,13 +30,14 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.BlockArrow = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.BlockArrow)).BeginInit();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Triforce", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(281, 12);
+            this.label1.Location = new System.Drawing.Point(24, 6);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(236, 41);
             this.label1.TabIndex = 0;
@@ -45,17 +46,28 @@
             // BlockArrow
             // 
             this.BlockArrow.ImageLocation = ".\\Images\\Blockers\\Arrow.gif";
-            this.BlockArrow.Location = new System.Drawing.Point(90, 117);
+            this.BlockArrow.Location = new System.Drawing.Point(37, 50);
             this.BlockArrow.Name = "BlockArrow";
-            this.BlockArrow.Size = new System.Drawing.Size(100, 50);
+            this.BlockArrow.Size = new System.Drawing.Size(52, 50);
             this.BlockArrow.TabIndex = 1;
             this.BlockArrow.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Triforce", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(37, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(52, 53);
+            this.button1.TabIndex = 2;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button1_MouseDown);
             // 
             // TrackerWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.BlockArrow);
             this.Controls.Add(this.label1);
             this.Name = "TrackerWindow";
@@ -68,6 +80,7 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox BlockArrow;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Z1RTracker/Form1.cs
+++ Z1RTracker/Form1.cs
@@ -8,9 +8,61 @@ namespace Z1RTracker
         public TrackerWindow()
         {
             InitializeComponent();
-            this.Text = "Z1R Tracker";
-            this.ForeColor = Color.Maroon;
+            Text = "Z1R Tracker";
+            ForeColor = Color.Maroon;
             BackColor = Color.DarkOliveGreen;
+        }
+
+        private void button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Button button = sender as Button;
+            switch(e.Button)
+            {
+                case MouseButtons.Left:
+                    Increment(button);
+                    break;
+                case MouseButtons.Right:
+                    Decrement(button);
+                    break;
+                case MouseButtons.Middle:
+                    Clear(button);
+                    break;
+
+            }
+        }
+
+        private void Increment(Button button)
+        {
+            if (button == null) return;
+            var currentText = button.Text;
+            if (int.TryParse(currentText, out int currentNumber))
+            {
+                button.Text = currentNumber >= 8 ? "" : (++currentNumber).ToString();
+            }
+            else
+            {
+                button.Text = "1";
+            }
+        }
+
+        private void Decrement(Button button)
+        {
+            if (button == null) return;
+            var currentText = button.Text;
+            if (int.TryParse(currentText, out int currentNumber))
+            {
+                button.Text = currentNumber <= 1 ? "" : (--currentNumber).ToString();
+            }
+            else
+            {
+                button.Text = "8";
+            }
+        }
+
+        private void Clear(Button button)
+        {
+            if (button == null) return;
+            button.Text = "";
         }
     }
 }
